afa904609997a220e76ec6e69800e79cdebae9e8
antoni phang <antoni.phang.95@gmail.com>
2026-08-16 09:24:36 +0700

    Reapply "Reapply "addMinimapAndMinimapControls""
    This reverts commit 9a0b135e4df8852b819074966c27f99fb4c9dada.

---
+++ Assets/Scripts/MinimapSc/MinimapActivator.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MinimapActivator : MonoBehaviour
+{
+    public InputActionReference minimapActivatorRef;
+    private Player player;
+    private bool mapActivated = false;
+    [SerializeField] private CanvasGroup minimapCanvasGroup;
+    void Start()
+    {
+        player = GetComponent<Player>();
+    }
+
+    private void OnEnable()
+    {
+        minimapActivatorRef.action.performed += TryToOpenMinimap;
+    }
+    private void OnDisable()
+    {
+        minimapActivatorRef.action.performed -= TryToOpenMinimap;
+    }
+    private void TryToOpenMinimap(InputAction.CallbackContext value)
+    {
+        if(mapActivated)
+        {
+            //disable map
+            mapActivated = !mapActivated;
+            minimapCanvasGroup.alpha = 0;
+            player.gatherInput.DisableMinimap();
+            if(player.playerStats.GetCurrentHealth() > 0)
+                player.gatherInput.EnablePlayerMap();
+        }
+        else
+        {
+            //enable map
+            if (player.playerStats.GetCurrentHealth() <= 0)
+                return;
+            mapActivated = !mapActivated;
+            minimapCanvasGroup.alpha = 1;
+            player.gatherInput.DisablePlayerMap();
+            player.gatherInput.EnableMinimap();
+        }
+    }
+}
