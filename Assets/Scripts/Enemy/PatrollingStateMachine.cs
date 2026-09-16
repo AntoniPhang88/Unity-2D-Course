222cd09e2171e0a852964f933c0888eeead88008
antoni phang <antoni.phang.95@gmail.com>
2026-09-16 21:22:52 +0700

    AddEnemyStateMachine

---
+++ Assets/Scripts/Enemy/PatrollingStateMachine.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class PatrollingStateMachine : EnemySimpleStateMachine
+{
+    public override void EnterIdle()
+    {
+        
+    }
+    public override void UpdateIdle()
+    {
+        
+    }
+}
